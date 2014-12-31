7fe36fa3e25cda8af4fe6260679d1055addaad7f
Michael Haselton <icereval@gmail.com>
2014-12-30 21:03:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author='',
     author_email='',
     url='https://github.com/CenterForOpenScience/aiohttpretty',
-    packages=find_packages(exclude=("tests*", )),
+    packages=find_packages(exclude=('tests*', )),
     include_package_data=True,
     install_requires=requirements,
     zip_safe=False,
@@ -21,7 +21,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
     ],
 )
