09fc6e58ef3a5d6df31b0e2a36aec0da67e04ab0
futa-ikeda <futa.ikeda@gmail.com>
2025-04-18 12:00:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.13',
     ],
 )
