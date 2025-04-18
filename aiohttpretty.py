5996601f316ed11d0376a6ef032482538fe7ec8b
futa-ikeda <futa.ikeda@gmail.com>
2025-04-18 12:01:20 -0400

    Update Sequence import; Add limit arg to StreamReader

--- aiohttpretty.py
+++ aiohttpretty.py
@@ -2,7 +2,7 @@ import sys
 import copy
 import json
 import asyncio
-import collections
+from collections.abc import Sequence
 from unittest.mock import Mock
 
 from yarl import URL
@@ -55,8 +55,9 @@ class _MockStream(StreamReader):
 
     def __init__(self, data):
 
+        DEFAULT_LIMIT = 2 ** 16
         protocol = BaseProtocol(Mock())
-        super().__init__(protocol)
+        super().__init__(protocol, limit=DEFAULT_LIMIT)
 
         self.size = len(data)
         self.feed_data(data)
@@ -117,7 +118,7 @@ class _AioHttPretty:
                 'Not making request. Go fix your test.'.format(**locals())
             )
 
-        if isinstance(response, collections.Sequence):
+        if isinstance(response, Sequence):
             try:
                 response = response.pop(0)
             except IndexError:
